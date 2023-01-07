f2ead5018465670da96d18c012f67965dafa361a
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2023-01-07 22:03:33 +0600

    redux store created and connected

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import Store from './redux/store/Store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/redux/store/Store.js
@@ -0,0 +1,7 @@
+// this will be the store for redux
+
+import { configureStore } from "@reduxjs/toolkit";
+
+export default configureStore({
+    reducer: {},
+});
